f4cc71a37c2c5d77c4cc3015cafac801c24d86ba
Paimon <n.h.shounen@gmail.com>
2022-03-07 23:33:23 +0700

    [NA] add POST method helper

--- models/request.py
+++ models/request.py
@@ -12,4 +12,28 @@ logger = logging.getLogger(__name__)
 
 class Request(models.AbstractModel):
     _name = 'paimon.request'
-    _base_url_config_key = ''
+    _base_url_config_key = ''    _base_url_config_key = ''
+
+    def _construct_url(self, endpoint):
+        base_url = config.get(self._base_url_config_key)
+        return urls.url_join(base_url, endpoint)
+
+    def POST(self, endpoint, data, header):
+        default_headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json', 
+            'Catch-Control': 'no-cache'
+        }
+        header = {**default_headers, **header}
+        url = self._construct_url(endpoint)
+        try:
+            response = requests.post(
+                url, data=data, headers=header)
+            return response.json()
+        except requests.exceptions.Timeout:
+            logger.error("request post timeout for url %s", url)
+            raise
+        except Exception:
+            logger.error("request post bad request response")
+            raise
+    
