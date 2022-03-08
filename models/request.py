ec5759ca056c95941d1a2e066786c68d96e23520
Paimon <n.h.shounen@gmail.com>
2022-03-08 22:52:07 +0700

    [NA] add get request

--- models/request.py
+++ models/request.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class Request(models.AbstractModel):
     _name = 'paimon.request'
-    _base_url_config_key = ''    _base_url_config_key = ''
+    _base_url_config_key = ''
 
     def _construct_url(self, endpoint):
         base_url = config.get(self._base_url_config_key)
@@ -37,3 +37,21 @@ class Request(models.AbstractModel):
             logger.error("request post bad request response")
             raise
     
+    def GET(self, endpoint, params = dict(), header = dict()):
+        default_headers = {
+            'Content-Type': 'application/json', 
+            'Accept': 'application/json', 
+            'Catch-Control': 'no-cache'
+        }
+        header = {**default_headers, **header}
+        url = self._construct_url(endpoint)
+        try:
+            response = requests.get(
+                url, params=params, headers=header)
+            return response.json()
+        except requests.exceptions.Timeout:
+            logger.error("request get timeout for url %s", url)
+            raise
+        except Exception:
+            logger.error("request get bad request response")
+            raise
