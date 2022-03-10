1cfc8a4d97bca6c9fdbb83592657e739c4d836cf
Paimon <n.h.shounen@gmail.com>
2022-03-10 18:55:57 +0700

    [NA] add method for post message slack

--- models/slack.py
+++ models/slack.py
@@ -1,4 +1,17 @@
+import requests
 from odoo import models
+from werkzeug import urls
+from ..helpers import const
 
 class Slack(models.AbstractModel):
     _name = 'paimon.slack'
+
+    def _construct_url(self, endpoint):
+        base_url = const.API_SLACK
+        return urls.url_join(base_url, endpoint)
+
+    def post_message(self, channel, message):
+        """
+        REF: https://api.slack.com/methods/chat.postMessage
+        """
+        method = 'chat.postMessage'
